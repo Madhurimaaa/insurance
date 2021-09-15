a8282fe8ab5a932012192fc15efa4ac06e4fd592
Madhurimaaa <madhurima.das@lntinfotech.com>
2021-09-15 17:06:53 +0530

    commit 2

---
+++ JunitProject/src/comm/lti/test/JungleTest.java
@@ -0,0 +1,46 @@
+package comm.lti.test;
+
+public class JungleTest {
+
+}
+class Tiger { }
+
+class Lion { }
+
+class Elephant {}
+
+
+ 
+
+class Cow {}
+
+class Goat { }
+
+class Hen { }
+
+
+ 
+
+class Parrot { }
+
+class Sparrow { }
+
+class Crow { }
+
+
+ 
+
+class Crocodile {}
+
+class Lizart { }
+
+class Snake { }
+
+
+ 
+
+class RosePlant { }
+
+class LillyPlant { }
+
+class LotusPlant { }
